9e8450c1b89e9a6357e13b1c3e8e7291888195f0
haripremrc <hari.prem24@gmail.com>
2024-03-30 16:34:02 +0530

    main - port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
